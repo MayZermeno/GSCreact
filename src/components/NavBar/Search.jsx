134114bda593b213a10756f8a682434b4a521d46
Mayra Zermeno <mayfassmich@gmail.com>
2023-08-28 20:10:49 -0600

    u.u

--- src/components/NavBar/Search.jsx
+++ src/components/NavBar/Search.jsx
@@ -1,30 +1,17 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {RiSearchLine } from "react-icons/ri";
 
-
-
-const Search = ({ onSearch }) => {
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const handleSearch = () => {
-    onSearch(searchQuery); // Llama a la función proporcionada por ItemListContainer
-  }; 
+const Search = () => {
+  
   return (
     <div className="search">
       <div className="container-fluid d-flex justify-content-center align-items-center">
         <div className="flex-wrap">
           <div id="search-form" className="d-flex mx-0" role="search">
-            <input className="form-control search_input" type="search"
-             placeholder="Buscar" aria-label="Search" id="busqueda"
-             onChange={(e) => setSearchQuery(e.target.value)}
-             value={searchQuery} />
-            <button className="btn btn-outline-warning search_icon " 
-            type="submit" id="boton-busqueda"
-            onClick={handleSearch} // Agrega el manejador de eventos onClick
-            >
+            <input className="form-control search_input" type="search" placeholder="Buscar" aria-label="Search" id="busqueda" />
+            <button className="btn btn-outline-warning search_icon " type="submit" id="boton-busqueda">
             <RiSearchLine className='icono-search'/>
             </button>
-          
           </div>
         </div>
       </div>
