e6fc827e193d6ab283af5893eaa479cc6fe8a066
Mayra Zermeno <mayfassmich@gmail.com>
2023-10-03 22:13:24 -0600

    actualizando la carpeta build con las animaciones

---
+++ build/precache-manifest.bb527feed24e8bc0206327d1651a3507.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "35af0a29f8db27b951b1",
+    "url": "/static/css/main.765bb03f.chunk.css"
+  },
+  {
+    "revision": "35af0a29f8db27b951b1",
+    "url": "/static/js/main.35af0a29.chunk.js"
+  },
+  {
+    "revision": "6f6f4986655de22842af",
+    "url": "/static/js/1.6f6f4986.chunk.js"
+  },
+  {
+    "revision": "1e27b8fc8af9f2ff4de8",
+    "url": "/static/js/2.1e27b8fc.chunk.js"
+  },
+  {
+    "revision": "15039e497f60efd69200",
+    "url": "/static/js/runtime~main.15039e49.js"
+  },
+  {
+    "revision": "2d1f8c7a275446b29316a0cb4d2b581e",
+    "url": "/static/media/SCarloslogo.2d1f8c7a.png"
+  },
+  {
+    "revision": "eb34d1b5d30073535dcb34e1fa5b786c",
+    "url": "/static/media/Congelato.eb34d1b5.png"
+  },
+  {
+    "revision": "1662599e12bc8263e6154c96e98bd829",
+    "url": "/static/media/Newlogo.1662599e.png"
+  },
+  {
+    "revision": "a7f919baddf5ac87e8bcc9ea504b7f35",
+    "url": "/static/media/NepicUs.a7f919ba.png"
+  },
+  {
+    "revision": "845d6918f8e9b8f8b9c611295c686504",
+    "url": "/static/media/bgmeat.845d6918.png"
+  },
+  {
+    "revision": "1d2577cf4941487a8eed12efeaa22521",
+    "url": "/static/media/bgchicken.1d2577cf.png"
+  },
+  {
+    "revision": "28450075bf9d4d9c5d8a1548dab81654",
+    "url": "/static/media/marmol.28450075.jpg"
+  },
+  {
+    "revision": "b60532fb53912edffff3d4370e0d8e96",
+    "url": "/index.html"
+  }
+];
