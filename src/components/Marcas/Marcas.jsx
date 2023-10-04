2dd9c20ec97ce0b210ff1c4c252324d0a70c1be5
Mayra Zermeno <mayfassmich@gmail.com>
2023-10-03 21:59:45 -0600

    se estan agregando animaciones

--- src/components/Marcas/Marcas.jsx
+++ src/components/Marcas/Marcas.jsx
@@ -5,6 +5,7 @@ import './Marcas.css'; // Importa tus estilos de Marcas.css
 import SCarlosLogo from './SCarloslogo.png';
 import CongelatoLogo from './Congelato.png';
 import datamarcas from './datamarcas.json';
+import { motion } from 'framer-motion';
 
 const Marcas = () => {
     const [selectedMarcaId, setSelectedMarca] = useState(null);
@@ -22,24 +23,29 @@ const Marcas = () => {
 
   return (
     <div className="marcas" id="marcas">
-      <div className="titulo d-flex justify-content-center">
+      <div className="titulo">
         <h2 className="brandtitle">NUESTRAS MARCAS</h2>
       </div>
+      <div className="titulo">        
+      <h4 className="subtitulo text-sm"> Clic en las imágenes para más información</h4></div>
       <div className="row brand-img">
         <div className="col-sm-12 col-md-6 alineadovertical">
           <div className="contenedor">
-            <img
+            <motion.img    whileHover={{ scale: 1.6 }} // Escala al hacer hover
+        whileTap={{ scale: 0.8 }}   // Escala al hacer clic (opcional)
               src={SCarlosLogo}
               className="img-fluid alico mb-2 w-50"
               alt="Alimentos San Carlos"
               style={{cursor:'pointer'}}
               onClick={() => handleMarcaClick("marca1")}
             />
+            
           </div>
         </div>
         <div className="col-sm-12 col-md-6 alineadovertical">
           <div className="contenedor">
-            <img
+            <motion.img    whileHover={{ scale: 1.3 }} // Escala al hacer hover
+        whileTap={{ scale: 0.8 }}   // Escala al hacer clic (opcional)
               src={CongelatoLogo}
               className="img-fluid congelato w-75 mb-1"
               alt="Congelato"
